a809ce2598d626dddcf008a54caf8715945d5a52
Alex Isaienko <thesonerik@gmail.com>
2019-08-18 01:11:03 +0300

    document_bloc.dart: initial sections support

--- lib/document_bloc.dart
+++ lib/document_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:collection/collection.dart';
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -28,11 +29,35 @@ class DocumentSize {
   static const empty = DocumentSize(0, 0);
 }
 
+@immutable
+class Section {
+  final String title;
+  final List<List<String>> data;
+
+  const Section(this.title, this.data);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Section &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          const DeepCollectionEquality().equals(data, other.data);
+
+  @override
+  int get hashCode => title.hashCode ^ data.hashCode;
+
+  static const Section empty = Section('', []);
+}
+
 class DocumentBloc implements Bloc {
   final String _source;
 
   final BehaviorSubject<List<List<String>>> _data = BehaviorSubject.seeded([]);
   List<List<String>> get data => _data.value;
+
+  Stream<List<int>> get sectionIndices => _data.map(_sectionIndices);
+  Stream<List<Section>> get sections => _data.map(_sections);
 
   Stream<DocumentSize> get size => _data.map(_size).distinct();
   Stream<int> get rows => size.map((s) => s.rows).distinct();
@@ -45,6 +70,9 @@ class DocumentBloc implements Bloc {
 
   DocumentBloc(this._source) {
     _init();
+    sections.listen((sections) {
+      print('sections: ${sections.length}');
+    });
   }
 
   void setCell(int row, int col, String value) {
@@ -95,11 +123,52 @@ String _cell(List<List<String>> data, int row, int col) {
   }
 }
 
+List<Section> _sections(List<List<String>> data) {
+  if (data.isEmpty) {
+    return const [Section.empty];
+  }
+
+  final result = <Section>[];
+  final sectionTitleColIndex = _sectionNameColumnIndexByRow(data[0]);
+
+  String lastSectionTitle = '';
+  List<List<String>> lastSectionData = [];
+  data.skip(1).forEach((row) {
+    final sectionTitle = row[sectionTitleColIndex];
+    if (sectionTitle?.isNotEmpty == true) {
+      if (lastSectionTitle?.isNotEmpty == true) {
+        result.add(Section(lastSectionTitle, lastSectionData));
+      }
+      lastSectionTitle = sectionTitle;
+      lastSectionData = [];
+    }
+    lastSectionData.add(row);
+  });
+  result.add(Section(lastSectionTitle, lastSectionData));
+  return result;
+}
+
+int _sectionCount(List<List<String>> data) =>
+    data.where((row) => _sectionNameColumnIndexByRow(row) >= 0).length;
+
+List<int> _sectionIndices(List<List<String>> data) {
+  final result = <int>[];
+  for (int i = 0; i < data.length; i++) {
+    if (_sectionNameColumnIndexByRow(data[i]) >= 0) {
+      result.add(i);
+    }
+  }
+  return result;
+}
+
 int _sectionNameColumnIndex(List<List<String>> data) {
   final firstRow = data?.firstWhere((_) => true, orElse: () => null);
   if (firstRow == null) {
     return -1;
   }
-  return -1; // TODO: section handling
-  return firstRow.indexWhere((e) => e.contains('comment'));
+//  return -1; // TODO: section handling
+  return _sectionNameColumnIndexByRow(firstRow);
 }
+
+int _sectionNameColumnIndexByRow(List<String> row) =>
+    row.indexWhere((e) => e.contains('comment'));
