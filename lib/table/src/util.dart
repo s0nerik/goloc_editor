30b958070b17bb7436c7b3677e128dadbff639ad
Alex Isaienko <thesonerik@gmail.com>
2019-09-20 01:05:42 +0300

    Overlap detection util

--- lib/table/src/util.dart
+++ lib/table/src/util.dart
@@ -1,25 +1,44 @@
+import 'dart:math';
+
 import 'package:meta/meta.dart';
 
-int overlapIndex({
+@immutable
+class Overlap {
+  final int index;
+  final double amount;
+
+  Overlap(this.index, this.amount);
+}
+
+List<Overlap> overlap({
   @required double draggableY,
   @required double draggableHeight,
   @required double tableScrollAmount,
   @required List<double> rowOffsets,
   @required List<double> rowHeights,
-  @required List<int> sectionTitlePositions,
 }) {
   assert(rowOffsets.length == rowHeights.length);
-  final headerHeight = rowHeights[0];
-  final pinnedSectionIndex = pinnedSectionTitleIndex(
-    tableScrollAmount: tableScrollAmount,
-    rowHeights: rowHeights,
-    sectionTitlePositions: sectionTitlePositions,
-  );
-  final pinnedSectionHeight = rowHeights[pinnedSectionIndex];
+  final draggableTop = tableScrollAmount + draggableY;
+  final draggableBottom = draggableTop + draggableHeight;
 
-  // TODO
+  final result = <Overlap>[];
 
-  return -1;
+  int i = 0;
+  double overlapAmount = 0;
+  while (i < rowOffsets.length) {
+    final overlapHeight = min(draggableBottom, rowOffsets[i] + rowHeights[i]) -
+        max(draggableTop, rowOffsets[i]);
+
+    if (overlapHeight > 0) {
+      overlapAmount = overlapHeight / rowHeights[i];
+      result.add(Overlap(i, overlapAmount));
+    } else if (overlapAmount > 0) {
+      break;
+    }
+    i++;
+  }
+
+  return result;
 }
 
 int pinnedSectionTitleIndex({
